40f8bffa8a16eb8a859e9e85e0c018a7321df83d
shawnxcong <22098740+shawnxcong@users.noreply.github.com>
2018-08-15 22:42:11 +0900

    add App of entrance

---
+++ backend/entrance.go
@@ -0,0 +1,24 @@
+package entrance
+
+import (
+	"entrance/backend/command"
+	"entrance/backend/job"
+)
+
+type App struct {
+	CommandService
+	JobService
+}
+
+func (app *App) AddCommand(name string, commandType int, segments string) int {
+	command, _ := command.New(name, commandType, segments)
+	return app.SaveCommand(command)
+}
+
+func (app *App) AddJob(cid int, arguments string) int {
+	command := app.Command(cid)
+	job, _ := job.New(command, arguments)
+	jobID := app.SaveJob(job)
+	go app.ExecuteJob(job)
+	return jobID
+}
