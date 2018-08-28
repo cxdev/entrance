03eee9814ecacb2808e6fc11b0e5a359ded17738
shawnxcong <22098740+shawnxcong@users.noreply.github.com>
2018-08-29 02:02:29 +0900

    Add QueryPlan for write data into DB by TX

--- backend/platform/db.go
+++ backend/platform/db.go
@@ -10,6 +10,16 @@ type DB struct {
 	*sql.DB
 }
 
+type Query struct {
+	Query string
+	Args  []interface{}
+}
+
+type QueryPlan struct {
+	db      *DB
+	queries []*Query
+}
+
 // Open returns a DB reference for a data source.
 func Open(dataSourceName string) (*DB, error) {
 	db, err := sql.Open("postgres", dataSourceName)
@@ -66,4 +76,31 @@ func createTable(conn *sql.DB) error {
 		}
 	}
 	return nil
+}
+
+func (db *DB) NewQueryPlan() *QueryPlan {
+	return &QueryPlan{db, []*Query{}}
+}
+
+func (db *DB) Exec(query string, args ...interface{}) (res sql.Result, err error) {
+	return db.NewQueryPlan().AppendQuery(query, args).Exec()
+}
+
+func (qp *QueryPlan) AppendQuery(query string, args ...interface{}) *QueryPlan {
+	qp.queries = append(qp.queries, &Query{query, args})
+	return qp
+}
+
+func (qp *QueryPlan) Exec() (res sql.Result, err error) {
+	tx, _ := qp.db.Begin()
+	defer tx.Rollback()
+
+	for _, query := range qp.queries {
+		if res, err = tx.Exec(query.Query, query.Args); err != nil {
+			return
+		}
+	}
+
+	err = tx.Commit()
+	return
 }
