53b980751057b970dfdf7fbce95f4734e05969c0
shawnxcong <22098740+shawnxcong@users.noreply.github.com>
2018-09-07 02:30:55 +0900

    Add method to setup entity data

--- backend/platform/entity.go
+++ backend/platform/entity.go
@@ -13,3 +13,9 @@ type BaseEntity struct {
 func (entity *BaseEntity) IsEntity() bool {
 	return BaseEntity{} != *entity
 }
+
+func (entity *BaseEntity) SetupEntity(Id int64, Created time.Time, Updated time.Time) {
+	entity.Id = Id
+	entity.Created = Created
+	entity.Updated = Updated
+}
