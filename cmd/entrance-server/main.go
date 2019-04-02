7c2ad094a89f78ef9069f0d8f4f7e390b70ee949
shawnxcong <22098740+shawnxcong@users.noreply.github.com>
2019-04-03 02:34:30 +0900

    improve db intial phase when the given db has data

--- cmd/entrance-server/main.go
+++ cmd/entrance-server/main.go
@@ -9,6 +9,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
@@ -20,11 +22,17 @@ type EntranceBackend struct {
 	execute.ExecuteService
 }
 
+type dbInitConfig struct {
+	CreateNewDB  bool `yaml:"create_new_db"`
+	InitCommands bool `yaml:"init_commands"`
+}
+
 type config struct {
-	Version     string             `yaml:"version"`
-	LogRootPath string             `yaml:"log_root_path"`
-	SqlitePath  string             `yaml:"sqlite_path"`
-	Commands    []entrance.Command `yaml:"commands"`
+	Version           string              `yaml:"version"`
+	LogRootPath       string              `yaml:"log_root_path"`
+	SqlitePath        string              `yaml:"sqlite_path"`
+	WhenDbExistConfig *dbInitConfig       `yaml:"when_db_exist"`
+	Commands          *[]entrance.Command `yaml:"commands"`
 }
 
 func initConfig(configPath string) (*config, error) {
@@ -51,9 +59,27 @@ func initEntranceBackend(db *gorm.DB, logRoot string) (*EntranceBackend, error) 
 	return &EntranceBackend{*storageservice, *executeservice}, nil
 }
 
+func getDBInitConfig(config *config) *dbInitConfig {
+	_, err := os.Stat(config.SqlitePath)
+	isDbExist := (err == nil)
+	if isDbExist {
+		return config.WhenDbExistConfig
+	}
+	return &dbInitConfig{false, true}
+}
+
+func removeAndBackup(path string) error {
+	// RFC3339: "2006-01-02T15:04:05Z07:00"
+	backupPostfix := time.Now().Format(time.RFC3339)
+	newLocation := path + "." + backupPostfix
+	err := os.Rename(path, newLocation)
+	return err
+}
+
 func initCommands(commands *[]entrance.Command, entranceBackend *EntranceBackend) error {
 	if commands != nil {
 		for _, command := range *commands {
+			// TODO: think more: create or update or need error?
 			_, err := entranceBackend.CreateCommand(command.Name, command.CommandType, command.CommandSegments)
 			if err != nil {
 				return err
@@ -75,6 +101,12 @@ func main() {
 	}
 	log.Println(config)
 
+	dbInitConfig := getDBInitConfig(config)
+
+	if dbInitConfig.CreateNewDB {
+		removeAndBackup(config.SqlitePath)
+	}
+
 	// db, _ := gorm.Open("sqlite3", "file::memory:?mode=memory")
 	db, err := gorm.Open("sqlite3", config.SqlitePath)
 	if err != nil {
@@ -87,9 +119,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	err = initCommands(&config.Commands, entranceApp)
-	if err != nil {
-		log.Fatal(err)
+	if dbInitConfig.InitCommands {
+		err = initCommands(config.Commands, entranceApp)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	router := router.NewRouter(entranceApp)
